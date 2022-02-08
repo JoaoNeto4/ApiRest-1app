14655b175bfcefffff7bbffca6be8ab87ca5edb8
João Neto4 <joaoquatlo@hotmail.com>
2022-02-07 21:47:24 -0300

    deploey heroku

--- src/main/java/com/produtos/apirest/config/SwaggerConfig.java
+++ src/main/java/com/produtos/apirest/config/SwaggerConfig.java
@@ -1,16 +1,32 @@
 package com.produtos.apirest.config;
 
+/*
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 //import org.springframework.core.type.filter.RegexPatternTypeFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
-import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+*/
+import springfox.documentation.service.ApiInfo;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.spring.web.plugins.Docket;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 @Configuration
-public class SwaggerConfig  {
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
 
 	    @Bean
 	    public Docket productApi() {
@@ -19,17 +35,29 @@ public class SwaggerConfig  {
 	          //.apis(RequestHandlerSelectors.any())
 	          .apis(RequestHandlerSelectors.basePackage("com.produtos.apirest"))
 	          //.paths(PathSelectors.any())
-	          .paths(PathSelectors.regex("/api.*")) 
-	          .build();                                           
+	          //.paths(PathSelectors.regex("/api.*")) 
+	          .build()
+	          .apiInfo(metaData());
 	    }
 	    
-	    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-	    registry.addResourceHandler("/swagger-ui.html")
-	    .addResourceLocations("classpath:/META-INF/resources/");
+	    private ApiInfo metaData() {
+		    return new ApiInfoBuilder()
+		        .title("Api Rest v1.0 2022")
+		        .description("Gerenciamento de aplicação java Web / Android ")
+		        .version("1.0.0")
+		        .license("Apache License Version 2.0")
+		        .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0.txt")
+		        .build();
+		  }
 	    
+	  @Override
+	  protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("swagger-ui.html")
+	        .addResourceLocations("classpath:/META-INF/resources/");
+
 	    registry.addResourceHandler("/webjars/**")
-	    .addResourceLocations("classpath:/META-INF/resources/webjars/");
-	    }
+	        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+	  }
 	 
 
 	    /*
